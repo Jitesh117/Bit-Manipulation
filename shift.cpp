71a78899db53f50417f49269524433d85f84f480
Jitesh117 <sahookumarjitesh@gmail.com>
2022-03-22 21:08:17 +0530

    added the first file

---
+++ shift.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   int n;
+    cout << "Enter any number: ";
+    cin >> n;
+    cout <<"Enter the value of i";
+    int i;
+    cin >> i;
+    cout << n << " << " << i<< " = "<< (n<<i);
+}
