65f18443c409fecaa20930478e2caa645b6d842f
Jitesh117 <sahookumarjitesh@gmail.com>
2022-03-23 06:21:25 +0530

    modified the no of set bits file

--- NoOfSetBits.cpp
+++ NoOfSetBits.cpp
@@ -7,10 +7,11 @@ int main()
     int count = 0;
     for(int i =1;i<=32;i++)
     {
-        n=n>>1;
+       
         // f = f << i;
         if(n&1!=0)
             count++;
+        n=n>>1;
         
     }
     cout <<endl<< count;
